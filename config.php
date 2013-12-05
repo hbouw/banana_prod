bf259be15aed370937d5a03dca128f219495b7ff
hbouw <h.bouwmeester@solum.nl>
2013-12-05 08:47:45 +0100

    config refers to banana_test

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ define('DB_USER', 'cropmapp');						//user of postgres database
 define('DB_PASS', 'U7z6yoxR38');					//password of postgres database
 define('DB_HOST', 'localhost');						//define host of database, in this case localhost because both php and postgres are on same server
 define('DB_PORT', '5432');							//specifies port on which the postgres db is
-define('DB_NAME', 'cropmapp_banana_prod');					//specifies the postgres database name
+define('DB_NAME', 'cropmapp_banana_test');					//specifies the postgres database name
 define('SECURED_LAYER','protected:states');			//specifies the secure (dummy) layer in geoserver, necessary for the login procedure 
 define('DOWNLOAD_LAYER','banana_systems');	    //specifies the layer in GeoServer) that is to be downloaded 
-define('GS_WORKSPACE','banana_prod');				//specifies workspace on geoserver
+define('GS_WORKSPACE','banana_test');				//specifies workspace on geoserver
 
 session_start();
 
